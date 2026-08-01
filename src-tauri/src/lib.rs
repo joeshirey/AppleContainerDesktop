7713f421a5ed7423f39809042ab14344252a218e
Joe Shirey <joeshirey@google.com>
2026-08-01 14:42:13 -0700

    feat: add the dialog plugin for picking a build context

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_dialog::init())
         .manage(BuildManager::default())
         .invoke_handler(tauri::generate_handler![
             list_containers,
